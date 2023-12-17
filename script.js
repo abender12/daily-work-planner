c9baccf043cd92eb54bb4662cbb818141fcb21ed
Alicen Bender <alicene.bender@gmail.com>
2023-12-17 16:20:43 -0500

    Added localStorage retrieval.

--- script.js
+++ script.js
@@ -3,28 +3,32 @@
 // in the html.
 
 $(function () {
-    // TODO: Add a listener for click events on the save button. This code should
-    // use the id in the containing time-block as a key to save the user input in
-    // local storage. HINT: What does `this` reference in the click listener
-    // function? How can DOM traversal be used to get the "hour-x" id of the
-    // time-block containing the button that was clicked? How might the id be
-    // useful when saving the description in local storage?
-    
+    // Store user input into localStorage
+    $('.saveBtn').on('click', function() {
+
+      var text = $(this).siblings('.description').val()
+      var time = $(this).parent().attr('id')
+
+      console.log('text:', text)
+      console.log('time:', time)
+
+      localStorage.setItem(time, text)
+    });
 
 
-    // TODO: Add code to apply the past, present, or future class to each time
-    // block by comparing the id to the current hour. HINTS: How can the id
-    // attribute of each time-block be used to conditionally add or remove the
-    // past, present, and future classes? How can Day.js be used to get the
-    // current hour in 24-hour time?
+    // Get the current hour.
     const currentHour = dayjs().hour()
-      // const testHour = dayjs().hour(13).hour()
+      const testHour = dayjs().hour(20).hour()
       console.log(currentHour)
+
+      // Try to make it so if it is before 9am, everything is green. If it is after 5pm, everything is grey.
       if (currentHour < 9) {
         document.querySelectorAll('.time-block').classList.add('future');
       } else if (currentHour > 17) {
         document.querySelectorAll('.time-block').classList.add('past');
-      } ;
+      };
+
+      // Every time the hour changes, the previous hours need to be marked as past and turn grey, the current hour needs to be red, and the future hours need to remain green.
       switch (currentHour) {
         case 9:
           document.getElementById('hour-9').classList.add('present')
@@ -136,10 +140,33 @@ $(function () {
       }
 
     
-    // TODO: Add code to get any user input that was saved in localStorage and set
-    // the values of the corresponding textarea elements. HINT: How can the id
-    // attribute of each time-block be used to do this?
+    // Retrieve information from localStorage
+    var hour9 = localStorage.getItem("hour-9");
+    $("#hour-9 .description").val(hour9);
   
+    var hour10 = localStorage.getItem("hour-10");
+    $("#hour-10 .description").val(hour10);
+  
+    var hour11 = localStorage.getItem("hour-11");
+    $("#hour-11 .description").val(hour11);
+    
+    var hour12 = localStorage.getItem("hour-12");
+    $("#hour-12 .description").val(hour12);
+    
+    var hour13 = localStorage.getItem("hour-1");
+    $("#hour-1 .description").val(hour13);
+    
+    var hour14 = localStorage.getItem("hour-2");
+    $("#hour-2 .description").val(hour14);
+    
+    var hour15 = localStorage.getItem("hour-3");
+    $("#hour-3 .description").val(hour15);
+    
+    var hour16 = localStorage.getItem("hour-4");
+    $("#hour-4 .description").val(hour16);
+    
+    var hour17 = localStorage.getItem("hour-5");
+    $("#hour-5 .description").val(hour17);
 
 
     // TODO: Add code to display the current date in the header of the page.
